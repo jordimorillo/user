7d714468dd7448904b42f68e7568399034ba514e
Jordi <jmdesarrollo82@gmail.com>
2024-03-16 20:50:45 +0100

    refactor: format spaces between methods

--- src/User/Domain/ValueObject/UserRepositoryInterface.php
+++ src/User/Domain/ValueObject/UserRepositoryInterface.php
@@ -8,12 +8,12 @@ use Source\User\Domain\Entity\User;
 
 interface UserRepositoryInterface
 {
-
     public function save(User $user): void;
 
     public function findById(UserId $userId): User;
 
     public function findByEmail(Email $email): User;
+
     public function exists(Email $email, Password $password): bool;
 
     public function delete(UserId $userId): void;
